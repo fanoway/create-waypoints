9daf6b9368b9cf0c3efd60e8cf4c32539037a2ec
fanoway <86997883+fanoway@users.noreply.github.com>
2023-03-13 15:07:05 -0700

    file upload possible

--- sar-waypoints/src/App.svelte
+++ sar-waypoints/src/App.svelte
@@ -1,13 +1,45 @@
 <script>
-  let name = "world";
+  import Dropzone from "svelte-file-dropzone";
+
+  let files = {
+    accepted: [],
+  };
+
+  function handleFilesSelect(e) {
+    const { acceptedFiles, fileRejections } = e.detail;
+    files.accepted = [...files.accepted, ...acceptedFiles];
+  }
+
+  function uploadfiles(_e) {
+    console.log(files.accepted);
+    for (let index in files.accepted) {
+      let file = files.accepted[index];
+      // upload
+    }
+  }
 </script>
 
 <main>
   <h1>sar-waypoints!</h1>
   <p>
-    Use this tool to upload GeoJSON from SAR topo to extract waypoints from
+    Use this tool to upload GeoJSON from SARTopo to extract waypoints from
     assignment areas
   </p>
+  <button on:click={uploadfiles} style="button">Convert</button>
+
+  <Dropzone accept="application/json" on:drop={handleFilesSelect} />
+  <ol>
+    {#each files.accepted as item}
+      <li>
+        <div>
+          <h2>{item.name}</h2>
+          {#await item.text() then text}
+            <p>{text}</p>
+          {/await}
+        </div>
+      </li>
+    {/each}
+  </ol>
 </main>
 
 <style>
